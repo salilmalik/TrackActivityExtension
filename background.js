9e05acaa6b92cef5b1f411f201e382cdecf6c0b9
salilmalik92@gmail.com <salilmalik92@gmail.com>
2019-06-24 14:40:25 -0700

    Added anchor tag tracking

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log(request);
-  console.log(request);
-  console.log(sender);
-  console.log(sendResponse);
+  var accountName;
+  chrome.cookies.get(
+    { url: "http://localhost:4200/", name: "accountName" },
+    function(cookie) {
+      if (cookie) {
+        accountName = cookie.value;
+        request.accountName = cookie.value;
+        var url = "http://localhost:3000/api/behaviour/" + accountName;
+        $.post(url, { type: request }, function(data) {
+          console.log(JSON.stringify(data));
+        });
+      }
+    }
+  );
 });
--- content.js
+++ content.js
@@ -1,7 +1,45 @@
 var anchorTags = document.getElementsByTagName("a");
 for (var i = 0; i < anchorTags.length; i++) {
-    anchorTags[i].addEventListener('click', function () {
-        chrome.runtime.sendMessage({ "event": "Click" });
+  anchorTags[i].addEventListener("click", function() {
+    var href = this.getAttribute("href");
+    var category;
+    var hrefArray = href.split("/");
+    if (hrefArray[1] === "questions") {
+      if (hrefArray[2] === "tagged") {
+        category = "Tag";
+      } else {
+        category = "Question";
+      }
+    } else if (hrefArray[1] === "users") {
+      category = "User Profile";
+    } else {
+      if (this.getAttribute("class") != "null") {
+        if (this.getAttribute("class" === "vote-up-off")) {
+          category = "Vote Up" + this.getAttribute("class");
+        } else if (this.getAttribute("class" === "vote-down-off")) {
+          category = "Vote down" + this.getAttribute("class");
+        }
+        category = this.getAttribute("class") + "is clicked";
+      } else {
+        category = "Clicked on " + href;
+      }
     }
-    )
-};
+    if ((href = "/question/ask/")) {
+      category = "Ask a Question";
+    }
+    chrome.runtime.sendMessage({
+      category: category,
+      event: "Click",
+      content: this.textContent
+    });
+  });
+}
+
+$(window).on("scroll", function() {
+  chrome.runtime.sendMessage({
+    category: "scroll",
+    event: "Scroll",
+    content: ""
+  });
+  $(window).off("scroll");
+});
